5725102207e2126cdee35544d1a2d4c0d76b7836
Yurii <yuriydorosh2005@gmail.com>
2024-05-14 14:50:49 +0300

    ADD: test table and resigter to admin

--- src/apps/tests/admin.py
+++ src/apps/tests/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.tests.models import Test
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "type", "created_at"]
+    list_filter = ["type"]
+    search_fields = ["name", "description"]
+    readonly_fields = ["created_at"]
+    fieldsets = (
+        (None, {
+            "fields": ("name", "description", "type", "created_at")
+        }),
+    )
+    ordering = ["-created_at"]
+    date_hierarchy = "created_at"
