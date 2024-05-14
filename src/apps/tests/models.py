46f6bfb54c18d3766608c36a576dd22be353b1cc
Yurii <yuriydorosh2005@gmail.com>
2024-05-14 14:48:03 +0300

    ADD: tests app

---
+++ src/apps/tests/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Test(models.Model):
+    class STATUS(models.TextChoices):
+        OK = "ok"
+        ERROR = "error"
+        
+    name = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    type = models.CharField(max_length=12, choices=STATUS.choices ,blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = "tests"
+        verbose_name = "Test"
+        verbose_name_plural = "Tests"
+
+    def __str__(self):
+        return self.name
--- src/config/components/apps.py
+++ src/config/components/apps.py
@@ -16,8 +16,7 @@ THIRD_PARTY = [
 ]
 
 LOCAL_APPS = [
-    # "apps.accounts",
-    # "apps.posts",
+    "apps.tests",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY + LOCAL_APPS
